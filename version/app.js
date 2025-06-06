d89a233f22726fdfc03134cadf0b6fb328f78be3
Romain Pelisse <belaran@redhat.com>
2025-06-06 08:44:46 +0200

    add dummy change

--- version/app.js
+++ version/app.js
@@ -4,7 +4,7 @@ app = express();
 var response;
 
 app.get('/', function (req, res) {
-
+    // Add dummy change
     response = 'This is version 1 of the app.' + '\n';
 
     //send the response to the client
